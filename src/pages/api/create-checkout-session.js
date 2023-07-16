6e8c0b21259e8479a2f1398d6dd5dcdc17d4b7cc
Shera <sanvishw.0080@gmail.com>
2023-07-16 11:09:07 +0530

    Check typo fixed

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -25,7 +25,7 @@ export default async (req, res) => {
     line_items: transformedItems,
     mode: "payment",
     success_url: `${process.env.HOST}/success`,
-    cancel_url: `${process.env.HOST}/checkout`,
+    cancel_url: `${process.env.HOST}/Check`,
     metadata: {
       email,
       images: JSON.stringify(items.map((item) => item.image)),
